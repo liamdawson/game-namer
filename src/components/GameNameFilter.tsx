dfeeb8af2b7e88048116f58e290ad670e4a43ed9
Liam Dawson <liam@ldaws.com>
2018-07-28 21:14:07 +1000

    clear filter when escape pressed

--- src/components/GameNameFilter.tsx
+++ src/components/GameNameFilter.tsx
@@ -19,6 +19,13 @@ export class GameNameFilter extends React.Component<Props, State> {
   private readonly onFilterChangedDebounced = lodash.debounce(() =>
     this.props.onFilterChanged && this.props.onFilterChanged(this.state.filter), 300);
 
+  private readonly resetFilterIfEscape = (e: React.KeyboardEvent) => {
+    if (e.keyCode == 27) {
+      this.setState({filter: ''});
+      this.onFilterChangedDebounced();
+    }
+  };
+
   private readonly filterChangedHandler = (e: ChangeEvent<HTMLInputElement>) => {
     let val = e.target.value;
 
@@ -41,8 +48,9 @@ export class GameNameFilter extends React.Component<Props, State> {
                   <Octicon icon={Search}/>
                 </span>
         </div>
-        <input id="game-name-filter" type="text" className="form-control form-control-sm"
-               placeholder="Filter games by name" value={this.state.filter} onChange={this.filterChangedHandler}/>
+        <input id="game-name-filter" type="search" className="form-control form-control-sm"
+               placeholder="Filter games by name" value={this.state.filter} onChange={this.filterChangedHandler}
+               onKeyUp={this.resetFilterIfEscape} />
       </div>
     )
   }
